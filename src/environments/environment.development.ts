c4a6772600f52a05a5bbfa2cd235a1f342b1d661
yousef132 <saady8454@gmail.com>
2025-09-24 14:39:21 +0300

    change env files

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    API_URL : 'https://localhost:7253/api/',
-   //API_URL : 'https://cps.premiumasp.net/api/',
-   SIGNALR_URL: 'https://localhost:7253/chatHub',
-    // SIGNALR_URL: 'https://cps.premiumasp.net/chatHub',
+   // API_URL : 'https://localhost:7253/api/',
+   API_URL : 'https://cps.premiumasp.net/api/',
+  // SIGNALR_URL: 'https://localhost:7253/chatHub',
+     SIGNALR_URL: 'https://cps.premiumasp.net/chatHub',
 };
